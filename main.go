49bc3be5a20db909460f8a0845475d461e930a44
MatthewScott-jpg <scottydotty333@gmail.com>
2021-10-09 11:51:44 -0400

    Formatting to have push/pull run sequentially

--- main.go
+++ main.go
@@ -23,9 +23,12 @@ var numInfected int64
 var totalRuns int64
 
 func (nm *NodeMap) pull(i int) {
+	nm.mu.Lock()
 	randnode := rand.Intn(3)
-	fmt.Println(nm.nodes[i].name, "Pulling from ", randnode)
-	nm.nodes[i].contactChannels[i] <- nm.nodes[randnode].message
+	if nm.nodes[i].message == 0 {
+		fmt.Println(nm.nodes[i].name, "Pulling from ", randnode)
+		nm.nodes[i].contactChannels[i] <- nm.nodes[randnode].message
+	}
 	select {
 	case x, ok := <-nm.nodes[randnode].contactChannels[i]:
 		if ok {
@@ -59,7 +62,7 @@ func (nm *NodeMap) pushReceiveMessage(i int) {
 				val.message = x
 				nm.nodes[i] = val
 				atomic.AddInt64(&numInfected, 1)
-				fmt.Println(i, "received", x)
+				//fmt.Println(i, "received", x)
 			}
 		}
 	default:
@@ -84,9 +87,9 @@ func main() {
 	nodes[1] = n1
 	nodes[2] = n2
 	nm = NodeMap{nodes: nodes}
-	nmlength := len(nm.nodes)
 
 	totalRuns = 0
+	//nmlength := len(nm.nodes)
 
 	//Implements push-pull protocol
 	/*if numInfected > int64(nmlength)/2 {
@@ -94,11 +97,9 @@ func main() {
 	//} else {
 
 	}*/
-
+	var wg sync.WaitGroup
 	//Implements push protocol
 	for numInfected < 3 {
-		var wg sync.WaitGroup
-		//nm.mu.Lock()
 		for i := 0; i < 3; i++ {
 			wg.Add(1)
 			go func(i int) {
@@ -112,14 +113,21 @@ func main() {
 			}(i)
 		}
 		wg.Wait()
-		//nm.mu.Unlock()
 		atomic.AddInt64(&totalRuns, 1)
 		time.Sleep(1 * time.Second)
 	}
+	fmt.Println("Push TR:", totalRuns)
+
+	//reset variables for pull run
+	totalRuns = 0
+	numInfected = 1
+	n1.message = 0
+	n2.message = 0
+	nm.nodes[1] = n1
+	nm.nodes[2] = n2
 
 	//Implements pull protocol
 	for numInfected < 3 {
-		var wg sync.WaitGroup
 		for i := 0; i < 3; i++ {
 			wg.Add(1)
 			go func(i int) {
@@ -136,5 +144,5 @@ func main() {
 	}
 
 	fmt.Println("All done")
-	fmt.Println("TR:", totalRuns)
+	fmt.Println("Pull TR:", totalRuns)
 }

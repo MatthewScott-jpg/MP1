8320f4042af48104c9d47bc90ac3b6b721ba66ba
MatthewScott-jpg <scottydotty333@gmail.com>
2021-10-10 15:34:08 -0400

    Comments, readme, adjusted code to be dynamic based on the number of nodes in the system

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 # MP1
+This project was created by Matthew Scott, Liam Potts, and Roland Afzelius. It creates structs resembling nodes in a network, then
+implements push, pull, and push-pull gossip to evaluate their performance.
 
+## How to run
+1. Download the repository and navigate to the folder with `main.go`, then enter `go run main.go`.
 
+2. The total number of runs required to complete each algorithm are printed. Print statements for intermediate steps are 
+commented out but can be uncommented to see what nodes are doing at each step. Each total run print takes between 5-10
+seconds to be called.
+
+## Design
+Although a central repository is used, this was to simplify the reusability of functions. Each node carries its own copy
+of the channels used for contacting nodes, and none know if the nodes they are contacting are infected or not, so they 
+are all operating independently. The channel maps were implemented in a way that the key associated with a map is the 
+name of the node that receives messages through it.
+#### Push
+Push protocol is implemented by having infected nodes push their message through the contactChannel map. After a brief 
+delay to maintain synchronicity, all nodes check their channel for messages, and uninfected nodes update their message
+if they received a new one.
+#### Pull
+Pull protocol is implemented by having uninfected nodes send their name through the nameChannel map to a random node. 
+Nodes then check their nameChannel for requests, and if they have received any, send their message through the 
+contactChannel using the name received as the key in the contactChannel map. Finally, all nodes check their contactChannel
+for messages and uninfected nodes update if they receive a new message.
+#### Push/Pull
+Push/pull protocol runs push protocol until half of the nodes in system are infected, then switches to pull protocol.
+
+## References
 read channel with default case: https://stackoverflow.com/questions/3398490/checking-if-a-channel-has-a-ready-to-read-value-using-go
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 //Node Each has a name, message, and a corresponding channel map and name map\\
-//Node Each node has its own channel map to ensure that no message is lost during Go Routines
+//Node Each node has its own channel map to affirm nodes can operate independently of one another
 type Node struct {
 	name, message   int
 	contactChannels map[int]chan int //for sending messages
@@ -22,26 +22,29 @@ type NodeMap struct {
 	mu    sync.Mutex
 }
 
+//nm holds the nodes created in a central store for easy access
+//numInfected and totalRuns declared as global variables, instantiated in main
 var nm NodeMap
 var nodes map[int]Node
 var numInfected int64
 var totalRuns int64
 
-//pullSendRequest
+//pullSendRequest has an uninfected node send a random node a message request
 func (nm *NodeMap) pullSendRequest(i int) {
 	if nm.nodes[i].message == 0 {
-		randNode := rand.Intn(3)
-		fmt.Println(nm.nodes[i].name, "Pulling from ", randNode)
+		randNode := rand.Intn(len(nm.nodes))
+		//fmt.Println(nm.nodes[i].name, "Pulling from ", randNode)
 		nm.nodes[i].nameChannels[randNode] <- nm.nodes[i].name
 	}
 }
 
+//pullSendMessage checks for pull requests in the node's channel, and sends contacting nodes its message
 func (nm *NodeMap) pullSendMessage(i int) {
-	for j := 0; j < len(nm.nodes); j++ {
+	for j := 0; j < len(nm.nodes); j++ { //loop through the channel to ensure it is cleared
 		select {
 		case contactor, ok := <-nm.nodes[i].nameChannels[i]:
 			if ok {
-				fmt.Println(i, "sending message to", contactor)
+				//fmt.Println(i, "sending message to", contactor)
 				nm.nodes[i].contactChannels[contactor] <- nm.nodes[i].message
 			}
 		default:
@@ -50,6 +53,8 @@ func (nm *NodeMap) pullSendMessage(i int) {
 	}
 }
 
+//pullReceiveMessage locks the NodeMap to prevent double counting, then if the node is uninfected, checks if it has
+//received the new message in the channel and if so, updates its message and increments the number infected
 func (nm *NodeMap) pullReceiveMessage(i int) {
 	nm.mu.Lock()
 	if nm.nodes[i].message == 0 {
@@ -72,16 +77,16 @@ func (nm *NodeMap) pullReceiveMessage(i int) {
 	nm.mu.Unlock()
 }
 
-//Implements pull protocol
+//pullProtocol runs the three pull methods in order for all nodes until all nodes are infected
 func pullProtocol(wg *sync.WaitGroup) {
-	for numInfected < 3 {
-		for i := 0; i < 3; i++ {
+	for int(numInfected) < len(nm.nodes) { //standardize integer types for comparison purposes
+		for i := 0; i < len(nm.nodes); i++ {
 			wg.Add(1)
 			go func(i int) {
 				defer wg.Done()
 				n := nm.nodes[i]
 				nm.pullSendRequest(n.name)
-				time.Sleep(1 * time.Second)
+				time.Sleep(1 * time.Second) //nodes sleep between method calls to maintain synchronicity
 				nm.pullSendMessage(n.name)
 				time.Sleep(1 * time.Second)
 				nm.pullReceiveMessage(n.name)
@@ -93,12 +98,16 @@ func pullProtocol(wg *sync.WaitGroup) {
 	}
 }
 
+//pushSendMessage chooses a random node to push a message to
+//Only infected nodes are allowed to call this function
 func (nm *NodeMap) pushSendMessage(i int) {
-	randNode := rand.Intn(3)
-	fmt.Println(nm.nodes[i].name, "Push: Sending to ", randNode)
+	randNode := rand.Intn(len(nm.nodes))
+	//fmt.Println(nm.nodes[i].name, "Push: Sending to ", randNode)
 	nm.nodes[i].contactChannels[randNode] <- nm.nodes[i].message
 }
 
+//pushReceiveMessage locks the NodeMap to prevent double counting, then checks its channel for messages.
+//If the node is not infected, it updates its message with the one in the channel.
 func (nm *NodeMap) pushReceiveMessage(i int) {
 	nm.mu.Lock()
 	select {
@@ -109,7 +118,7 @@ func (nm *NodeMap) pushReceiveMessage(i int) {
 				tmpNode.message = newMessage
 				nm.nodes[i] = tmpNode
 				atomic.AddInt64(&numInfected, 1)
-				fmt.Println(i, "received", newMessage)
+				//fmt.Println(i, "received", newMessage)
 			}
 		}
 	default:
@@ -118,10 +127,10 @@ func (nm *NodeMap) pushReceiveMessage(i int) {
 	nm.mu.Unlock()
 }
 
-//Implements push protocol
+//pushProtocol runs the two push methods in order for all nodes until all nodes are infected
 func pushProtocol(wg *sync.WaitGroup) {
-	for numInfected < 3 {
-		for i := 0; i < 3; i++ {
+	for int(numInfected) < len(nm.nodes) {
+		for i := 0; i < len(nm.nodes); i++ {
 			wg.Add(1)
 			go func(i int) {
 				defer wg.Done()
@@ -139,13 +148,12 @@ func pushProtocol(wg *sync.WaitGroup) {
 	}
 }
 
-//Runs Push or Pull protocol based on the ratio of numInfected and total nodes.
+//pushPullProtocol runs Push or Pull protocol based on the ratio of numInfected and total nodes.
 func pushPullProtocol(wg *sync.WaitGroup) {
-	for numInfected < 3 {
-		print(len(nm.nodes)/2)
+	for int(numInfected) < len(nm.nodes) {
 		if numInfected < int64(len(nm.nodes)/2) {
 			pushProtocol(wg)
-		}else {
+		} else {
 			pullProtocol(wg)
 		}
 		atomic.AddInt64(&totalRuns, 1)
@@ -153,7 +161,7 @@ func pushPullProtocol(wg *sync.WaitGroup) {
 	}
 }
 
-//resets totalRuns and numInfected to starting value,
+//resetVariables resets totalRuns and numInfected to starting value,
 func resetVariables(totalNodes int) {
 	totalRuns = 0
 	numInfected = 1
@@ -161,7 +169,7 @@ func resetVariables(totalNodes int) {
 		select {
 		case _, ok := <-nm.nodes[0].contactChannels[i]:
 			if ok {
-				fmt.Println("Clearing Channel")
+				//fmt.Println("Clearing Channel")
 			}
 		default:
 			break //handles case where channel is empty
@@ -178,11 +186,13 @@ func main() {
 	rand.Seed(time.Now().UnixNano()) //Ensure we get a different seed for random each time
 	var wg sync.WaitGroup
 
+	numNodes := 4 //Dynamically set the number of nodes in the system
+
 	contacts := make(map[int]chan int)
 	names := make(map[int]chan int)
-	for i := 0; i < 3; i++ {
-		contacts[i] = make(chan int, 3)
-		names[i] = make(chan int, 3)
+	for i := 0; i < numNodes; i++ {
+		contacts[i] = make(chan int, numNodes)
+		names[i] = make(chan int, numNodes)
 	}
 	n0 := Node{0, 1, contacts, names}
 
@@ -190,22 +200,23 @@ func main() {
 	nodes[0] = n0
 	nm = NodeMap{nodes: nodes}
 
-	resetVariables(3)
-	//Calls push protocol function
+	//Set variables for push run
+	resetVariables(numNodes)
+	//Call push protocol function, then print the results
 	pushProtocol(&wg)
-	fmt.Println("Push TR:", totalRuns)
+	fmt.Println("Push Total Runs:", totalRuns)
 
-	//reset variables for pull run
-	resetVariables(3)
+	//Reset variables for pull run
+	resetVariables(numNodes)
 
-	//Calls pull protocol function
+	//Call pull protocol function, then print the results
 	pullProtocol(&wg)
-	fmt.Println("Pull TR:", totalRuns)
+	fmt.Println("Pull Total Runs:", totalRuns)
 
-	//reset variables for push-pull-run
-	resetVariables(3)
+	//Reset variables for push-pull-run
+	resetVariables(numNodes)
 
-	//Calls push-pull protocol function
+	//Call push-pull protocol function, then print the results
 	pushPullProtocol(&wg)
-	fmt.Println("Push-Pull TR:", totalRuns)
+	fmt.Println("Push-Pull Total Runs:", totalRuns)
 }
